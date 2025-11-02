c984d94aef97ca976ef7cd3de5ac0a7941a4abc0
Hibi_10000 <Hibi-10000@outlook.jp>
2025-11-03 00:51:15 +0900

    upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "eslint/config";
+
+import globals from "globals";
+import eslint from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default defineConfig([
+  {
+    ignores: [
+      "node_modules",
+      "test",
+      "eslint.config.js",
+    ],
+  },
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: "double",
+    semi: true,
+    jsx: false,
+  }),
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        //...globals.browser,
+        ...globals.commonjs,
+        ...globals.node,
+      },
+      ecmaVersion: 2016,
+    },
+    extends: [
+      eslint.configs.recommended,
+    ],
+    rules: {
+      "@stylistic/spaced-comment": 0,
+    },
+  },
+]);
