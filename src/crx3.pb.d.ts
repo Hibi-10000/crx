5a3f5182e2b870d600fa1c880cdb4d2f0186414a
Hibi_10000 <Hibi-10000@outlook.jp>
2025-11-25 01:48:21 +0900

    add crx3.pb.d.ts

---
+++ src/crx3.pb.d.ts
@@ -0,0 +1,23 @@
+import type Pbf from "pbf";
+
+type CrxFileHeader = {
+    sha256_with_rsa?: AsymmetricKeyProof[];
+    sha256_with_ecdsa?: AsymmetricKeyProof[];
+    signed_header_data?: Uint8Array;
+};
+export function readCrxFileHeader(pbf: Pbf, end?: number): CrxFileHeader;
+export function writeCrxFileHeader(obj: CrxFileHeader, pbf: Pbf): void;
+
+interface AsymmetricKeyProof {
+    public_key?: Uint8Array;
+    signature?: Uint8Array;
+}
+export function readAsymmetricKeyProof(pbf: Pbf, end?: number): AsymmetricKeyProof;
+export function writeAsymmetricKeyProof(obj: AsymmetricKeyProof, pbf: Pbf): void;
+
+interface SignedData {
+    crx_id?: Uint8Array;
+}
+export function readSignedData(pbf: Pbf, end?: number): SignedData;
+export function writeSignedData(obj: SignedData, pbf: Pbf): void;
+
